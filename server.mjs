d9cd10444068ef5038516d04c3fce36a62b1a45f
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-11-09 14:52:58 +0000

    project setup

---
+++ server.mjs
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const result = dotenv.config()
+if (result.error) {
+    console.log('Can\'t load environment variables now')
+    process.exit(0)
+}
+
+const app = express()
+
+app.use(express.json(), cors())
+
+app.get('/', (req, res) => {
+    res.end(`<h1>Typing app endpoint</h1>`)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on http://localhost:${process.env.PORT}`)
+})
